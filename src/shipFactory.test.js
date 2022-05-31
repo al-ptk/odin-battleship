03a9265ec7a960cc0c37264ea224429fa1cf444f
Alan Patrick <alan.patrick.fv@gmail.com>
2022-05-30 21:59:18 -0300

    Set ships to be built vertically

--- src/shipFactory.test.js
+++ src/shipFactory.test.js
@@ -1,13 +1,14 @@
 import shipFactory from './shipFactory';
 
 describe('Testing the ship object interface', () => {
+  const horShip = shipFactory(6, false, 3, 10);
+  const verShip = shipFactory(6, true, 3, 10);
+  
   test('Ship initializes with horizontal direction correctly', () => {
-    const horShip = shipFactory(6, false, 3, 10);
     expect(horShip.getBoundaries()).toStrictEqual(['6', '7', '8']);
   });
 
   test('Ship initializes with vertical direction correctly', () => {
-    const verShip = shipFactory(6, true, 3, 10);
     expect(verShip.getBoundaries()).toStrictEqual(['6', '16', '26']);
   });
 });
