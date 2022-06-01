44baa30d130097ac1cf90818188ba0ae8d4edc84
Alan Patrick <alan.patrick.fv@gmail.com>
2022-06-01 14:22:00 -0300

    Add some svg art

--- src/index.js
+++ src/index.js
@@ -6,3 +6,22 @@ import titleScreen from './ui-modules/titleScreen';
 const root = document.querySelector('#app');
 
 root.appendChild(nextPlayer());
+
+/**
+ * Backlog:
+ *  - Make the cv, for fuck's sake
+ *  - Create screen controller called puppeteer, because it sounds cool
+ *  - Create the board screen
+ *  - Create a game over over screen
+ *    - How and when should I announce game over?
+ */
+  
+// function ScreenCallbackFactory(screen) {
+//   return function () {
+//     root.appendChild(screen());
+//   };
+// }
+
+// const toPickPlayer = ScreenCallbackFactory(pickPlayersScreen);
+// const toNextPlayer = ScreenCallbackFactory(nextPlayer)figma
+;
